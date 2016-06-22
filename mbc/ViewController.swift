7a16bca25466ed4cb72e1b5ee4dc9a957fb4a8bc
admin <admin@chu1e1adeimac.local>
2016-06-22 17:56:06 +0800

    Initial Commit

---
+++ mbc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mbc
+//
+//  Created by admin on 16/6/22.
+//  Copyright © 2016年 20141105047. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
